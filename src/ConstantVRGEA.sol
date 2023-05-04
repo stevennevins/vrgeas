8dccea44aeda2cb10710da1bf173866d9c9d33bc
steven nevins <steven.nevins.js@gmail.com>
2023-05-04 09:01:56 -0400

    implementation of a normal ranked auction

---
+++ src/ConstantVRGEA.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {VRGEA} from "src/VRGEA.sol";
+import {unsafeWadDiv} from "solmate/utils/SignedWadMath.sol";
+
+abstract contract LinearVRGEA is VRGEA {
+    int256 public immutable targetTime;
+
+    /// @dev _targetTime must be in dayWads
+    constructor(
+        uint256 _startTime,
+        uint256 _reservePrice,
+        uint256 _minBidIncrease,
+        int256 _targetTime
+    ) VRGEA(_startTime, _minBidIncrease, _reservePrice) {
+        targetTime = _targetTime;
+    }
+
+    function getTargetSaleTime(
+        int256
+    ) public view virtual override returns (int256) {
+        return targetTime;
+    }
+}
